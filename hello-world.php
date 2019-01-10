c0dd35c7351e41975cd9c9dd0aba7243f08dd0c6
Purich <6666@ait.nsw.edu.au>
2019-01-10 02:55:19 +0000

    basic startup

--- hello-world.php
+++ hello-world.php
@@ -1,10 +1,10 @@
 <html><body>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
 
 echo 'Hello world from Cloud9!';
+//hi purish how fuck are you doing?
+//fuck u damon chen
+//WTF?
 
 ?>
 </body>
